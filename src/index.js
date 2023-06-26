6183e8aa852c03163ef00238b71dad680865b278
jaafar mohammad <jafarmh777@gamil.com>
2023-06-26 23:47:35 +0200

    lfs

--- src/index.js
+++ src/index.js
@@ -5,16 +5,22 @@ import App from './App';
 import {BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
-
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let persistor = persistStore(store)
+
 root.render(
 
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+     <PersistGate persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+     </PersistGate>
   </Provider>
+
   
 );
 
